e83925b3d534abe83657d79cde96e5bb66fb9659
fjalafoo <fjalafoo@gmail.com>
2022-10-10 22:20:56 +0300

    what I understood so far

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
-const apiURL='https://icanhazdadjoke.com/'
-fetch(apiURL, {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        "Content-Type" : "Application/json",
-    }
-})
+let generateBtn = document.getElementById('generateBtn')
+let result = document.getElementById('resultJoke')
 
+const addJoke = (joke) => {
+    result.innerHTML=joke
+}
+
+const fetchJokes = () => {
+    const apiURL='https://icanhazdadjoke.com/'
+    fetch(apiURL, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type" : "Application/json",
+        }
+    })
+    .then(fetchedJokesObj=>{
+        return fetchedJokesObj.json()
+    })
+    .then(jsonJokes=>{
+        console.log(jsonJokes.results)
+        jsonJokes.results.forEach(addJoke)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+}
+
+
+
+
+const clear = () => {
+   result.innerHTML = ""
+}
+
+
+generateBtn.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    clear()
+    fetchJokes()
+})

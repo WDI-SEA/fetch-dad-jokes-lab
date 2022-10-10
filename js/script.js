e3cfbbfb4d20d81f246ff2c7daae7da321f6cd32
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-10-10 20:29:56 +0300

    Deliverable Done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log("Hello World")
+const jokesForm = document.querySelector('#jokesForm')
+
+const theJoke = document.querySelector('#theJoke')
+
+
+const addJokeToHTML = (joke) => {
+    theJoke.innerText = joke
+}
+
+
+const generateJoke = () => {
+
+    let randomJokeURL = "https://icanhazdadjoke.com"
+    
+    fetch(randomJokeURL, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type" : "Application/json",
+        }
+    })
+    .then(fetchedJokesObj => {
+        return fetchedJokesObj.json()
+    }) 
+    .then(jsonJokes => {
+        addJokeToHTML(jsonJokes.joke)
+    })
+    .catch(err => {
+        console.log("No jokes found", err)
+    })
+
+}
+
+
+jokesForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    generateJoke() 
+})

8978a19219a86b57cdc57a299ed7f659d40b3236
fjalafoo <fjalafoo@gmail.com>
2022-10-10 21:35:18 +0300

    basics

---
+++ app.js
@@ -0,0 +1,9 @@
+const apiURL='https://icanhazdadjoke.com/'
+fetch(apiURL, {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        "Content-Type" : "Application/json",
+    }
+})
+

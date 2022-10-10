f631d414817ab663a39805c34f9af38944c86d8f
Sharifaaz <Sharifaalzayed96@gmail.com>
2022-10-10 23:31:53 +0300

    Idon't know why it doesn't work for me

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+
+
+// const jokeDiv = document.getElementById("displayed-joke")
+// const jokesButton = document.getElementById ("jokeBtn")
+
+// // function daddyJokes (){
+//  const jokeFcn =()=> {
+//  fetch('https://icanhazdadjoke.com/', {
+//         headers: {
+//           'Accept': 'application/json'
+//       }
+//     })
+      
+//         .then((jokesData)=>{
+//           return jokesData.json();
+//         })
+//         .then((jsonData)=>{
+          
+//             jokeDiv.innerText= jsonData.joke
+//         })
+//     }
+    
+//     jokesButton.addEventListener("click", ()=>{
+//         jokeFcn()
+        
+//     })
+
+let laughters = new Audio ("mixkit-laughing-teenager-428.wav")
+
+const jokeFetch = ()=>{
+    fetch('https://icanhazdadjoke.com/', {
+       headers: {
+       'Accept': 'application/json'
+}})
+
+ .then((fetchedObj)=>{
+   return fetchedObj.json();
+ })
+ .then((jokes)=>{
+   jokes.joke
+   textJoke.innerText=jokes.joke 
+ })}
+
+
+let textJoke = document.querySelector("#displayed-joke")
+const jokeBtn = document.querySelector("#jokeBtn")
+jokeBtn.addEventListener("click", ()=>{
+   jokeFetch()
+   laughters.play()
+})

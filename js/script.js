787216128e2622e1c0ab122c0f6c80a7aecfcf63
shaikhaalbuflasa <shaikhasaad86@gmail.com>
2022-10-11 08:35:10 +0300

    fetch lab solution

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let DadJoke = document.getElementById("Dad-Joke")
+let theJoke = document.getElementById("the-joke")
+
+let sound= new Audio('mixkit-cartoon-voice-laugh-343.wav')
+
+const fetchjoke = () => {
+fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      'Accept': 'application/json'
+    }
+})
+
+.then((fetchJoke)=>{
+    return fetchJoke.json();
+})
+.then((jsonJoke)=>{
+    // console.log(jsonJoke.joke)
+    jsonJoke.joke
+    theJoke.innerText=jsonJoke.joke
+
+})
+.catch(err=>{
+    console.log(err)
+})
+
+}
+
+
+DadJoke.addEventListener('click', ()=>{
+    fetchjoke()
+    sound.play()
+})
+
+const clearList = () => {
+   
+    theJoke.innerText = ""
+}
+
+
+
+
+

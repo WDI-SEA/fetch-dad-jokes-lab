5180996371fa0c3b004b814cf62615026dcf0fb7
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-10-11 09:06:02 +0300

    silly jokes found

---
+++ script.js
@@ -0,0 +1,20 @@
+const jokeFetch = ()=>{
+    fetch('https://icanhazdadjoke.com/', {
+       headers: {
+       'Accept': 'application/json'
+}})
+
+ .then((fetchedObj)=>{
+   return fetchedObj.json();
+ })
+ .then((jokes)=>{
+   jokes.joke
+   textJoke.innerText=jokes.joke 
+ })}
+
+
+let textJoke = document.querySelector("#joke-display")
+const jokeBtn = document.querySelector("#jokeBtn")
+jokeBtn.addEventListener("click", ()=>{
+   jokeFetch()
+})
